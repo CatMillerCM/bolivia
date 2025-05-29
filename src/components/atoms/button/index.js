fadba9067c4b399cc57dcf38db7bb6d410aa17a9
Cat Miller <catmiller.cm@gmail.com>
2025-05-28 19:20:55 -0500

    disabled take photo button unless template selected

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 import styles from './button.module.css';
 
-const Button = ({ onClick, label }) => {
+const Button = ({ onClick, label, disabled }) => {
   return (
-    <button className={styles.button} onClick={onClick}>{label}</button>
+    <button className={`${styles.button} ${disabled ? styles.disabled : ''}`} onClick={onClick} disabled={disabled}>{label}</button>
   )
 };
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired
 };
 
 export { Button };
