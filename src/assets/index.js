6c10f011b3272600d95c93e36a0081bbebfe5203
Cat Miller <catmiller.cm@gmail.com>
2025-05-28 19:23:29 -0500

    add template alts in objects

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,34 @@
 import photoExample from './bolivia.jpg';
 
-export { photoExample };
+const saltFlatsImages = [
+  {
+    image: photoExample,
+    alt: 'Group running away from a dinosaur perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: 'Female in the yoga pose atop a beer bottle perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: 'Female holding a figure in the palm of her hand perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: 'Female leaning against a beer can perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: 'Female stomping down on a figure perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: 'Group punching a dinosaur perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: photoExample,
+    alt: '2 females atop a beer bottle perspective photo on the Uyuni Salt Flats'
+  }
+]
+
+export { photoExample, saltFlatsImages };
