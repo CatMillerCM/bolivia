6cdff0c0a1e3c5086ac6e2b590b3124a1d8e39d8
Cat Miller <catmiller.cm@gmail.com>
2025-06-14 19:42:52 -0500

    add my salt flat images to carousel

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,34 +1,30 @@
-import photoExample from './bolivia.jpg';
+import carousel1 from './carousel-1.webp';
+import carousel2 from './carousel-2.webp';
+import carousel3 from './carousel-3.webp';
+import carousel4 from './carousel-4.webp';
+import carousel5 from './carousel-5.webp';
 
 const saltFlatsImages = [
   {
-    image: photoExample,
-    alt: 'Group running away from a dinosaur perspective photo on the Uyuni Salt Flats'
-  },
-  {
-    image: photoExample,
-    alt: 'Female in the yoga pose atop a beer bottle perspective photo on the Uyuni Salt Flats'
-  },
-  {
-    image: photoExample,
-    alt: 'Female holding a figure in the palm of her hand perspective photo on the Uyuni Salt Flats'
-  },
-  {
-    image: photoExample,
-    alt: 'Female leaning against a beer can perspective photo on the Uyuni Salt Flats'
-  },
-  {
-    image: photoExample,
-    alt: 'Female stomping down on a figure perspective photo on the Uyuni Salt Flats'
-  },
-  {
-    image: photoExample,
+    image: carousel1,
     alt: 'Group punching a dinosaur perspective photo on the Uyuni Salt Flats'
   },
   {
-    image: photoExample,
-    alt: '2 females atop a beer bottle perspective photo on the Uyuni Salt Flats'
+    image: carousel5,
+    alt: 'Female in the yoga pose atop a beer bottle perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: carousel2,
+    alt: 'Female holding a figure in the palm of her hand perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: carousel4,
+    alt: 'Female leaning against a beer can perspective photo on the Uyuni Salt Flats'
+  },
+  {
+    image: carousel3,
+    alt: 'Female stomping down on a figure perspective photo on the Uyuni Salt Flats'
   }
-]
+];
 
-export { photoExample, saltFlatsImages };
+export { saltFlatsImages };
